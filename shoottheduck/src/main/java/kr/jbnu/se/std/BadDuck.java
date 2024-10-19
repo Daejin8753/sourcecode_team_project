8e3852075d3df5320a80486999f5770bbd24f69f
Deajin8753 <jinyee8753@gmail.com>
2024-10-19 14:31:56 +0900

    주석 한국어로 변경 -대진

--- shoottheduck/src/main/java/kr/jbnu/se/std/BadDuck.java
+++ shoottheduck/src/main/java/kr/jbnu/se/std/BadDuck.java
@@ -5,6 +5,6 @@ import java.awt.image.BufferedImage;
 public class BadDuck extends Duck {
     public BadDuck(int x, int y, int speedX, int speedY, BufferedImage badDuckImg) {
         super(x, y, speedX, speedY, badDuckImg);
-        this.score = -50; // 플레이어가 쏘면 -50점을 잃음 } @Override public void Draw(Graphics2D g2d) { super.Draw(g2d); // 기본 오리와 같은 방식으로 그리기 } @Override public void Update() { super.Update(); // 기본 오리와 같은 방식으로 업데이트 }
+        this.score = -50; // 플레이어가 쏘면 -50점을 잃음
     }
 }
