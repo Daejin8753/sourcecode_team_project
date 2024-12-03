6dc6ff35b79b6b6effbf03f3b12a42d4ada5e998
liufu <liufuqiang933@jbnu,ac,kr>
2024-12-03 12:51:38 +0900

    수정

--- shoottheduck/src/main/java/kr/jbnu/se/std/FastDuck.java
+++ shoottheduck/src/main/java/kr/jbnu/se/std/FastDuck.java
@@ -1,15 +1,9 @@
 package kr.jbnu.se.std;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class FastDuck extends Duck {
-    public FastDuck(int x, int y, double speed, int line, BufferedImage duckImg) {
-        super(x, y, speed * 2, line, duckImg); // 오리의 속도를 2배로 설정
-    }
-
-    @Override
-    public void Draw(Graphics2D g2d) {
-        g2d.drawImage(duckImg, x, y, null); // 일반 오리와 동일한 그리기 방식
+    public FastDuck(int x, int y, double speed, int line, BufferedImage fastDuckImg) {
+        super(x, y, speed * 2, line, fastDuckImg); // 오리의 속도를 2배로 설정
     }
 }
